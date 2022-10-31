03d59742f3d92a70cc247b60f0874bc427a0b8ff
Jm <jmsbuensuceso@iskolarngbayan.pup.edu.ph>
2022-10-31 22:51:39 +0800

    Added functionality to add payment record.

--- public/views/invoice_payments_add.php
+++ public/views/invoice_payments_add.php
@@ -2,6 +2,58 @@
   include '../models/header.html';
   include '../models/navbar.html'; ?>
 
+<main id="main" class="main">
+
+  <div class="pagetitle">
+    <h1>Add Payment Record</h1>
+    <nav>
+      <ol class="breadcrumb">
+        <li class="breadcrumb-item"><a href="dashboard.html">Home</a></li>
+        <li class="breadcrumb-item active">Invoice</li>
+      </ol>
+    </nav>
+  </div><!-- End Page Title -->
+
+  <section class="section payments-add">
+    <div class="row">
+      <div class="col-mb-8">
+
+        <div class="card">
+          <div class="card-body pt-4">
+              
+          <form id="add-payment">
+                  <div class="row mb-3">
+                      <label for="payment_ref" class="col-sm-2 col-form-label">Payment Ref. ID</label>
+                      <div class="col-sm-10">
+                          <input type="text" class="form-control" id="payment_ref" required>
+                      </div>
+                  </div>
+
+                  <div class="row mb-3">
+                      <label for="amount_paid" class="col-sm-2 col-form-label">Amount Paid</label>
+                      <div class="col-sm-10">
+                          <input type="text" class="form-control" id="amount_paid" required>
+                      </div>
+                  </div>
+
+                <div class ="text-center">
+                  <button type="submit" class="btn btn-primary">Create Record</button>
+                </div>
+
+
+              </form>
+
+          </div>
+        </div>
+
+      </div>
+    </div>
+  </section>
+
+</main><!-- End #main -->
+
+<a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+
   <!-- Vendor JS Files -->
   <script src="../assets/vendor/apexcharts/apexcharts.min.js"></script>
   <script src="../assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
