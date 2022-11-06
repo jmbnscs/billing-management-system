66c7c2825bfe02a827dfe3a1ec917a07a9d02c45
Jm <jmsbuensuceso@iskolarngbayan.pup.edu.ph>
2022-11-06 20:04:51 +0800

    Added modal config.

--- public/js/customers.js
+++ public/js/customers.js
@@ -50,6 +50,30 @@ async function getCustomers () {
     }
 }
 
+// Modal for Customer List
+async function setModal () {
+    var exampleModal = document.getElementById('modalDialogScrollable')
+    exampleModal.addEventListener('show.bs.modal', function (event) {
+      // Button that triggered the modal
+      var button = event.relatedTarget
+      // Extract info from data-bs-* attributes
+      var recipient = button.getAttribute('data-bs-whatever')
+      // If necessary, you could initiate an AJAX request here
+      // and then do the updating in a callback.
+      //
+      try_lang(recipient);
+    
+      async function try_lang (account_id) {
+        // Update the modal's content.
+        var modalTitle = exampleModal.querySelector('.modal-title')
+        //   var modalBodyInput = exampleModal.querySelector('.modal-body input')
+    
+        modalTitle.textContent = 'New message to ' + recipient
+        //   modalBodyInput.value = recipient
+    }
+    })
+}
+
 // Add Customer JS
 const getID = async () => {
     const result = await generateAccountID();
