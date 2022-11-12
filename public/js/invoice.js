9c41a0150be50bebc399f14e23f6a7b6d587520b
Jm <jmsbuensuceso@iskolarngbayan.pup.edu.ph>
2022-11-13 01:42:46 +0800

    Added functionality to invoice page.

--- public/js/invoice.js
+++ public/js/invoice.js
@@ -31,12 +31,174 @@ $(document).ready(function () {
         }
     }
     else {
-        getInvoices();
+        setInvoicePage();
     }
 });
+// Global Functions
+function displaySuccessMessage() {
+    const msg = sessionStorage.getItem('save_message');
+    if (msg !== null) {
+        toastr.success(sessionStorage.getItem("save_message"));
+        sessionStorage.removeItem("save_message");
+    }
+}
+
+let create_fn, edit_fn, update_fn, delete_fn;
+function setButtons() {
+    // create_fn = document.getElementById('create-new');
+    edit_fn = document.getElementById('edit-btn');
+    update_fn = document.getElementById('update-data');
+    // delete_fn = document.getElementById('delete-data');
+}
+
+function setData (id, data, setAttr, bool) {
+    $(id).val(data);
+    $(id).attr(setAttr, bool);
+}
 
 // View Invoices JS
-async function getInvoices () {
+async function setInvoicePage () {
+    displaySuccessMessage();
+    setInvoiceTable();
+    setButtons();
+    setUpdateModal();
+    
+    $("#editModal").on("hidden.bs.modal", function () {
+        $('#save-btn').attr('disabled', true);
+        $('#edit-btn').attr('disabled', false);
+    });
+
+    update_fn.onsubmit = (e) => {
+        e.preventDefault();
+        updateData();
+    };
+
+    // Set View Invoice Modal
+    async function setUpdateModal () {
+        var updateModal = document.getElementById('editModal')
+        updateModal.addEventListener('show.bs.modal', async function (event) {
+    
+            var button = event.relatedTarget;
+            var invoice_id = button.getAttribute('data-bs-whatever');
+            let data = await getInvoiceData(invoice_id);
+            let customer_data = await getCustomerData(data.account_id);
+            let inc_id;
+
+            // Display Invoice Details
+            $('#invoice_id').val(data.invoice_id);
+            $('#account_id').val(data.account_id);
+            $('#billing_period_start').val(data.billing_period_start);
+            $('#billing_period_end').val(data.billing_period_end);
+            $('#disconnection_date').val(data.disconnection_date);
+            $('#previous_bill').val(data.previous_bill);
+            $('#previous_payment').val(data.previous_payment);
+            $('#balance').val(data.balance);
+            $('#secured_cash').val(data.secured_cash);
+            $('#subscription_amount').val(data.subscription_amount);
+            $('#prorated_charge').val(data.prorated_charge);
+            $('#installation_charge').val(data.installation_charge);
+            $('#total_bill').val(data.total_bill);
+            $('#invoice_status_id').val(data.invoice_status_id);
+
+            if (data.invoice_status_id == 1) {
+                toggleInputData('readonly', true);
+                $('#edit-btn').attr('hidden', true);
+                $('#save-btn').attr('hidden', true);
+            }
+            else {
+                toggleInputData('disabled', true);
+                $('#amount_paid').val(null);
+            }
+
+            function toggleInputData (setAttr, bool) {
+                setData('#payment_reference_id', data.payment_reference_id, setAttr, bool);
+                setData('#amount_paid', data.amount_paid, setAttr, bool);
+                setData('#payment_date', data.payment_date, setAttr, bool);
+            }
+
+            for (var i = 0; i < data.length; i++) {
+                if (inclusion_id == data[i].inclusion_id) {
+                    inc_id = i;
+                }
+            }
+
+            var modalTitle = updateModal.querySelector('.modal-title');
+            modalTitle.textContent = customer_data.first_name + ' ' + customer_data.last_name + ' - ' + data.invoice_id;
+    
+            // Form Submits -- onclick Triggers
+            edit_fn.onclick = (e) => {
+                e.preventDefault();
+                $('#save-btn').attr('disabled', false);
+                $('#edit-btn').attr('disabled', true);
+                toggleInputData('disabled', false);
+                $('#amount_paid').val(null);
+            };
+        });
+    }
+
+    async function updateData() {
+        const account_id = $('#account_id').val();
+        const invoice_id = $('#invoice_id').val();
+        const payment_reference_id = $('#payment_reference_id').val();
+        const amount_paid = $('#amount_paid').val();
+        const payment_date = $('#payment_date').val();
+    
+        let url = DIR_API + 'invoice/update.php';
+        const updateDataResponse = await fetch(url, {
+            method : 'PUT',
+            headers : {
+                'Content-Type' : 'application/json'
+            },
+            body : JSON.stringify({
+                'account_id' : account_id,
+                'payment_reference_id' : payment_reference_id,
+                'amount_paid' : amount_paid,
+                'payment_date' : payment_date
+            })
+        });
+
+        url = DIR_API + 'payment/create.php';
+        const addPaymentResponse = await fetch(url, {
+            method : 'POST',
+            headers : {
+                'Content-Type' : 'application/json'
+            },
+            body : JSON.stringify({
+                'amount_paid' : amount_paid,
+                'payment_reference_id' : payment_reference_id,
+                'payment_date' : payment_date
+            })
+        });
+
+        const content = await updateDataResponse.json();
+        const payment_content = await addPaymentResponse.json();
+
+        url = DIR_API + 'payment/update_tagged.php';
+        const updateTaggedResponse = await fetch(url, {
+            method : 'POST',
+            headers : {
+                'Content-Type' : 'application/json'
+            },
+            body : JSON.stringify({
+                'account_id' : account_id,
+                'invoice_id' : invoice_id,
+                'payment_id' : payment_content.payment_id
+            })
+        });
+
+        const tagged_content = await updateTaggedResponse.json();
+    
+        if (content.message == 'Invoice Updated' && payment_content.message == 'Payment Record Created' && tagged_content.message == 'Payment Tagged') {
+            sessionStorage.setItem('save_message', "Payment Updated Successfully.");
+            window.location.reload();
+        }
+        else {
+            toastr.error("Payment was not updated.");
+        }
+    }
+}
+
+async function setInvoiceTable() {
     let url = DIR_API + 'views/invoice.php';
     let invoice_data;
     var t = $('#invoice-table').DataTable();
@@ -62,8 +224,29 @@ async function getInvoices () {
                 <td>${invoice_data[i].disconnection_date}</td>
                 <td>&#8369; ${invoice_data[i].total_bill}</td>
                 <td><span class="badge ${tag}">${invoice_data[i].status}</span></td>
+                <td><button type="button" class="btn btn-outline-primary" data-bs-toggle="modal" data-bs-target="#editModal" data-bs-whatever="${invoice_data[i].invoice_id}"><i class="ri ri-eye-fill"></i></button></td>
             </tr>
         `)).draw(false);
+    }
+}
+
+async function getInvoiceData(invoice_id) {
+    let url = DIR_API + 'invoice/read_single.php?invoice_id=' + invoice_id;
+    try {
+        let res = await fetch(url);
+        return await res.json();
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+async function getCustomerData(account_id) {
+    let url = DIR_API + 'customer/read_single.php?account_id=' + account_id;
+    try {
+        let res = await fetch(url);
+        return await res.json();
+    } catch (error) {
+        console.log(error);
     }
 }
 
@@ -156,23 +339,44 @@ async function addPayment () {
     });
 
     const payment_content = await addPaymentResponse.json();
-    
+
     if (payment_content.message == 'Payment Record Created') {
         toastr.success('Payment Created Successfully.');
         setTimeout(function(){
             window.location.replace('../views/invoice_payments.php');
             // window.location.reload();
-         }, 2000);
+            }, 2000);
     }
     else {
         toastr.error(payment_content.message);
         setTimeout(function(){
             window.location.reload();
-         }, 2000);
+            }, 2000);
     }
 }
 
-function setAddPaymentPage () {
+// Update Tagged Payment
+async function updateTagged (payment_id) {
+    const account_id = $('#account_id').val();
+    const invoice_id = $('#invoice_id').val();
+
+    let url = DIR_API + 'payment/update_tagged.php';
+    const updateTaggedResponse = await fetch(url, {
+        method : 'POST',
+        headers : {
+            'Content-Type' : 'application/json'
+        },
+        body : JSON.stringify({
+            'account_id' : account_id,
+            'invoice_id' : invoice_id,
+            'payment_id' : payment_id
+        })
+    });
+
+    return await updateTaggedResponse.json();
+}
+
+async function setAddPaymentPage () {
     const add_record = document.getElementById('add-payment');
 
     // Form Submits -- onclick Triggers
--- public/views/invoice.php
+++ public/views/invoice.php
@@ -13,7 +13,7 @@
     </nav>
   </div><!-- End Page Title -->
 
-  <!-- Recent Sales -->
+  <!-- Invoice Table -->
   <div class="col-12">
     <div class="card recent-sales overflow-auto">
       <br>
@@ -26,6 +26,7 @@
               <th scope="col">Disconnection Date</th>
               <th scope="col">Total Bill</th>
               <th scope="col">Status</th>
+              <th scope="col">View</th>
             </tr>
           </thead>
           <tbody id="invoice-data">
@@ -35,7 +36,158 @@
       </div>
 
     </div>
-  </div><!-- End Recent Sales -->
+  </div><!-- End Invoice Table -->
+
+<!-- View Invoice Modal -->
+<form id="update-data">
+  <!-- Modal Dialog Scrollable -->
+  <div class="modal fade" id="editModal" tabindex="-1">
+      <div class="modal-dialog modal-fullscreen modal-dialog-centered modal-lg">
+        <div class="modal-content">
+
+          <!-- Modal Header -->
+          <div class="modal-header">
+            <h5 class="modal-title"></h5>
+            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+          </div>
+
+          <!-- Modal Body -->
+          <div class="modal-body row g-3">
+             <div class="container px-4">
+              <div class="row mb-3">
+                <!-- Left -->
+                <div class="col-sm-8">
+
+                  <div class="card mt-2">
+                    <div class="card-header">Invoice Details</div>
+                    <div class="card-body">
+                      <div class="row pt-2">
+                        <div class="col-md-8">
+                          <label for="invoice_id" class="form-label">Invoice ID</label>
+                          <input type="text" class="form-control" id="invoice_id" readonly>
+                        </div>
+                        <div class="col-md-4">
+                          <label for="account_id" class="form-label">Account ID</label>
+                          <input type="text" class="form-control" id="account_id" readonly>
+                        </div>
+                      </div>
+
+                      <div class="row pt-4"> 
+                        <div class="col-md-4">
+                          <label for="billing_period_start" class="form-label">Billing Period Start</label>
+                          <input type="date" class="form-control" id="billing_period_start" readonly>
+                        </div>
+                        <div class="col-md-4">
+                          <label for="billing_period_end" class="form-label">Billing Period End</label>
+                          <input type="date" class="form-control" id="billing_period_end" readonly>
+                        </div>
+                        <div class="col-md-4">
+                          <label for="disconnection_date" class="form-label">Disconnection Date</label>
+                          <input type="date" class="form-control" id="disconnection_date" readonly>
+                        </div>
+                      </div>
+
+                      <div class="row pt-4"> 
+                        <div class="col-md-6">
+                          <label for="previous_bill" class="form-label">Previous Bill</label>
+                          <input type="text" class="form-control" id="previous_bill" readonly>
+                        </div>
+                        <div class="col-md-6">
+                          <label for="previous_payment" class="form-label">Previous Payment</label>
+                          <input type="text" class="form-control" id="previous_payment" readonly>
+                        </div>
+                      </div>
+
+                      <div class="row pt-4"> 
+                        <div class="col-md-6">
+                          <label for="balance" class="form-label">Balance</label>
+                          <input type="text" class="form-control" id="balance" readonly>
+                        </div>
+                        <div class="col-md-6">
+                          <label for="secured_cash" class="form-label">Secured Cash</label>
+                          <input type="text" class="form-control" id="secured_cash" readonly>
+                        </div>
+                      </div>
+
+                      <div class="row pt-4"> 
+                        <div class="col-md-4">
+                          <label for="subscription_amount" class="form-label">Subscription Amount</label>
+                          <input type="text" class="form-control" id="subscription_amount" readonly>
+                        </div>
+                        <div class="col-md-4">
+                          <label for="prorated_charge" class="form-label">Prorated Charge</label>
+                          <input type="text" class="form-control" id="prorated_charge" readonly>
+                        </div>
+                        <div class="col-md-4">
+                          <label for="installation_charge" class="form-label">Installation Charge</label>
+                          <input type="text" class="form-control" id="installation_charge" readonly>
+                        </div>
+                      </div>
+
+                      <div class="row pt-4">
+                        <div class="col-md-8">
+                          <label for="total_bill" class="form-label">Total Bill</label>
+                          <input type="text" class="form-control" id="total_bill" readonly>
+                        </div>
+                        <div class="col-md-4">
+                          <label for="invoice_status_id" class="form-label">Invoice Status</label>
+                          <input type="text" class="form-control" id="invoice_status_id" readonly>
+                        </div>
+                      </div> 
+                    </div>
+                  </div>
+
+                </div> <!-- End Left -->
+
+                <!-- Right -->
+                <div class="col-sm-4">
+                  <div class="row pt-4">
+                    <h5 class="text-center">Payment Details</h5>
+                    <div class="col-md-12 pt-4">
+                      <label for="payment_reference_id" class="form-label">Payment Reference ID</label>
+                      <input type="text" class="form-control" id="payment_reference_id" required>
+                    </div>
+
+                    <div class="col-md-12 pt-4">
+                      <label for="amount_paid" class="form-label">Amount Paid</label>
+                      <input type="text" class="form-control" id="amount_paid" required>
+                    </div>
+
+                    <div class="col-md-12 pt-4">
+                      <label for="payment_date" class="form-label">Payment Date</label>
+                      <input type="date" class="form-control" id="payment_date" required>
+                    </div> 
+
+                    <div class="mt-5 d-grid gap-2 col-8 mx-auto">
+                      <button type="button" class="btn btn-outline-success" id="edit-btn">Add Payment Info</button>
+                    </div>
+
+                    <div class="mt-5 d-grid gap-2 col-8 mx-auto">
+                      <button type="submit" class="btn btn-outline-primary" id="save-btn" disabled>Save Changes</button>
+                    </div>
+
+                    <div class="mt-5 d-grid gap-2 col-8 mx-auto">
+                      <button type="button" class="btn btn-outline-secondary" data-bs-dismiss="modal" id="cls-btn">Close</button>
+                    </div>
+
+                  </div>
+                </div>
+              </div>
+             </div>
+          </div>
+          <!-- End Modal Body -->
+
+          <!-- Modal Footer -->
+          <!-- <div class="modal-footer">
+            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+            <button type="button" class="btn btn-primary" id="edit-btn">Edit</button>
+            <button type="submit" class="btn btn-success" id="save-btn" disabled>Save Changes</button>
+          </div> -->
+        </div>
+      </div>
+  </div>
+</form> <!-- End View Invoice Modal -->
+
 </main><!-- End #main -->
 
   <!-- Vendor JS Files -->
