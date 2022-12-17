153cc82103e147f3ab685890f90ff2c7862816e3
Jm <jmsbuensuceso@iskolarngbayan.pup.edu.ph>
2022-12-18 07:56:18 +0800

    ud

--- app/includes/curl.init.php
+++ app/includes/curl.init.php
@@ -1,6 +1,6 @@
 <?php
 // Declare Main DIR
-if (!defined("DIR_API")) define("DIR_API", "http://localhost/gstech_api/api/");
+if (!defined("DIR_API")) define("DIR_API", "http://gstechbms-online.preview-domain.com/gstech_api/api/");
 
 // Inititiate curl
 $ch = curl_init();
