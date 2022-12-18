35822bd45ea5b7817bcb5a7a55f0a37767e96f7f
Jm <jmsbuensuceso@iskolarngbayan.pup.edu.ph>
2022-12-19 00:09:03 +0800

    test

--- public/js/customers.js
+++ public/js/customers.js
@@ -180,7 +180,7 @@ async function setCustomerPage () {
 async function setAddCustomerPage () {
     const add_customer = document.getElementById('add-customer');
 
-    if (session_storage.getItem('account_id') == null) {
+    if (sessionStorage.getItem('account_id') == null) {
         sessionStorage.setItem('account_id', await generateID('check/account_id.php?account_id=', "", 8));
     }
     $('#account_id').val(sessionStorage.getItem('account_id'));
